a9c8873d98436b2f8f55273f627a2c28fbe47849
nicolas <nicolas.bazireau@hotmail.fr>
2023-09-15 09:55:13 +0200

    modification code javascript

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,6 +1,8 @@
 const apiURL = "http://localhost:5678/api/works";
+const galleryDiv = document.querySelector(".gallery");
+let originalData = [];
+// console.log(originalData);
 
-// Options de la requête
 const requestOptions = {
   method: "GET",
   headers: {
@@ -8,38 +10,74 @@ const requestOptions = {
   },
 };
 
-// Sélectionnez la div avec la classe "gallery" où vous voulez afficher les projets
-const galleryDiv = document.querySelector(".gallery");
-
-// appel à l'API avec fetch
-fetch(apiURL, requestOptions)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`La requête a échoué avec le statut ${response.status}`);
-    }
-    return response.json();
-  })
-    
-    
-  .then((data) => {
-    // Créez du contenu HTML pour chaque projet
-    const projectsHTML = data
-      .map(
-        (work) => `
-      <div class="project">
-        <img src="${work.imageUrl}" alt="${work.title}">
-        <p>Catégorie : ${work.name}</p>
-      </div>
-    `
-      )
-      .join("");
-
-    // Ajoutez le contenu HTML à la div avec la classe "gallery"
-    galleryDiv.innerHTML = projectsHTML;
-  })
-    
-    
-  .catch((error) => {
+function fetchProjects() {
+  fetch(apiURL, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `La requête a échoué avec le statut ${response.status}`
+        );
+      }
+      return response.json();
+    })
+    .then((data) => {
+      originalData = data;
+      displayProjects(data);
+    })
+    .catch((error) => {
       console.error("Une erreur s'est produite :", error);
-  
+    });
+}
+
+function displayProjects(data) {
+  let projectsHTML = "";
+  data.forEach((work) => {
+    projectsHTML += `
+      <div class="projet">
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <p>${work.title}</p>
+      </div>
+    `;
   });
+  galleryDiv.innerHTML = projectsHTML;
+}
+
+function filterProjectsByCategory(categoryId) {
+  const filteredData = originalData.filter(
+    (work) => work.categoryId === categoryId
+  );
+  displayProjects(filteredData);
+}
+
+const boutonTous = document.querySelector(".btn-1");
+const boutonObjet = document.querySelector(".btn-2");
+const boutonApartements = document.querySelector(".btn-3");
+const boutonHotelRestaurant = document.querySelector(".btn-4");
+const boutonLogin = document.querySelector(".login");
+
+boutonTous.addEventListener("click", () => {
+  displayProjects(originalData);
+});
+
+boutonObjet.addEventListener("click", () => {
+  filterProjectsByCategory(1);
+});
+
+boutonApartements.addEventListener("click", () => {
+  filterProjectsByCategory(2);
+});
+
+boutonHotelRestaurant.addEventListener("click", () => {
+  filterProjectsByCategory(3);
+});
+
+
+boutonLogin.addEventListener("click", () => {
+  console.log("vous avez apuyer sur login");
+});
+
+// Appel initial pour récupérer tous les projets
+fetchProjects();
+
+
+
