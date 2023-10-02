1046d7fc3345470638a7406b112d3fc1f65cc7fe
nicolas <nicolas.bazireau@hotmail.fr>
2023-10-02 16:09:56 +0200

    modification add-projet ajout image+afichage+ajoutcategorie

--- FrontEnd/modify.js
+++ FrontEnd/modify.js
@@ -94,9 +94,79 @@ function deleteProjectById(projectId) {
 const addProjects = document.querySelector(".add-projets")
 const Modale = document.querySelector(".Modale")
 const boutonAddPhoto = document.querySelector(".btn-add-picture");
+/////masquer modale pour afficher page ajout photo
 boutonAddPhoto.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("vous avez cliquer sur ajouter une photo");
+  // console.log("vous avez cliquer sur ajouter une photo");
   Modale.style.display = "none";
   addProjects.style.display = "block";
 });
+
+/////retourner a la fenetre supressiond de projet
+const arrowReturne = document.querySelector(".fa-arrow-left")
+arrowReturne.addEventListener("click", () => {
+  Modale.style.display = "block";
+  addProjects.style.display = "none";
+});
+
+////au clique sur le button ajout photo ouvrire input type:file
+const uploadButton = document.getElementById("upload-button");
+const imageUpload = document.getElementById("image-upload");
+const containerPhoto = document.querySelector(".container-photo");
+
+
+
+uploadButton.addEventListener("click", () => {
+  imageUpload.click(); 
+});
+
+// Gérer le changement de fichier sélectionné
+imageUpload.addEventListener("change", () => {
+  const file = imageUpload.files[0]; // Récupérer le fichier sélectionné par l'utilisateur
+  if (file) {
+    const imgElement = new Image();
+    imgElement.src = URL.createObjectURL(file);
+    imgElement.alt = "Image sélectionnée";
+
+    // Effacez tout le contenu existant de la div "container-photo"
+    containerPhoto.innerHTML = "";
+
+    // Ajoutez l'élément image à la div "container-photo"
+    containerPhoto.appendChild(imgElement);
+  } else {
+    // Effacez tout le contenu de la div "container-photo" si aucun fichier n'est sélectionné
+    containerPhoto.innerHTML = "";
+  }
+});
+
+const categorySelect = document.getElementById("category-select");
+
+// Effectuer une requête GET vers l'API
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((data) => {
+    // Créer un tableau pour stocker les noms de catégories uniques
+    const uniqueCategories = {};
+
+    // Parcourir les données de l'API
+    data.forEach((item) => {
+      const categoryName = item.category.name;
+      uniqueCategories[categoryName] = true;
+    });
+
+    // Ajouter les options à l'élément select
+    for (const categoryName in uniqueCategories) {
+      if (uniqueCategories.hasOwnProperty(categoryName)) {
+        const option = document.createElement("option");
+        option.value = categoryName;
+        option.textContent = categoryName;
+        categorySelect.appendChild(option);
+      }
+    }
+  })
+  .catch((error) => {
+    console.error(
+      "Erreur lors de la récupération des données depuis l'API : ",
+      error
+    );
+  });
